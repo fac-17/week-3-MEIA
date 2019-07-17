0ea0326808235a036e877b1d6aa0977020122d07
andy <andy.william.mcdonald@gmail.com>
2019-07-17 17:12:26 +0100

    work in progress for image adjusted css for position of image and text placeholder for image

--- feature-meme.js
+++ feature-meme.js
@@ -4,3 +4,10 @@ request("https://corporatebs-generator.sameerkumar.website/", function(response)
   p.textContent = JSON.stringify(response.phrase)
   sentence.appendChild(p);
 });
+
+request("https://picsum.photos/v2/list?page=2&limit=100", function(response) {
+  const image = document.querySelector('.image');
+  let i = document.createElement('IMG')
+  // i.innerHTML = JSON.stringify(response.download_url)
+  image.appendChild(i);
+});
