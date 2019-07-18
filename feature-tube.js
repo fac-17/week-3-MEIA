93dc5b2acb5438d56587626ed9ea365aea21899f
Jan Skowronski <crianonim@gmail.com>
2019-07-18 12:42:49 +0100

    Tube new style

--- feature-tube.js
+++ feature-tube.js
@@ -12,6 +12,8 @@ request("https://api.tfl.gov.uk/line/mode/tube/status", function(response) {
     const status = line.lineStatuses[0];
     let statusSeverityDescription = status.statusSeverityDescription;
     const reason = status.reason;
+    if (line.id=="jubilee")
+     statusSeverityDescription="Severe Delays";
     //gets status class from helper function
     let statusClass = getStatusClass(statusSeverityDescription);
     let li = document.createElement("li");
